7b30d01bf76e2c8c5e7170abedadacc2d3741f67
Alba Alcaniz <albaalcanizmoya@gmail.com>
2020-06-25 17:32:30 +0200

    Improve the series minigame
    I have included the image with the series on the Series minigame,
    and the spaces where you can set the correct value for the series
    I have also included the alphabet, so that the player can get the
    message, however it is still not working as I want to.
    write the number that follows the series. I have also included the
    alphabet, however it is still not working as I want to.

--- Experiments.py
+++ Experiments.py
@@ -1,20 +1,58 @@
-# from tkinter import *
-import tkinter as tk
-root=tk.Tk()
-def retrieve_input():
-    inputValue=textBox.get("1.0","end-1c")
-    print(inputValue)
-    if inputValue == 'Fernando':
-        print('Congrats!')
-        exit()
+from tkinter import *
 
-    # return inputValue
+master = Tk()
 
-textBox=tk.Text(root, height=2, width=10)
-textBox.pack()
-buttonCommit=tk.Button(root, height=1, width=10, text="Commit", 
-                    command=lambda: retrieve_input())
-#command=lambda: retrieve_input() >>> just means do this when i press the button
-buttonCommit.pack()
+e = Entry(master)
+e.pack()
 
-root.mainloop()
+e.focus_set()
+
+def callback():
+    print(e.get())
+
+b = Button(master, text="get", width=10, command=callback)
+b.pack()
+
+mainloop()
+
+#####################
+
+
+# import tkinter as tk
+# from PIL import ImageTk, Image
+
+# master=tk.Tk()
+# font_ = 32; wid = 3
+
+# # Image with the numerical series
+# img1 = ImageTk.PhotoImage(image = Image.open("figures/2_series.png"))
+# image = tk.Label(master, image = img1)
+# image.grid(row=0, column=0, columnspan=1, rowspan=5)
+
+# # Entries where the player writes the numbers following the series
+# tk.Entry(master, font=("Helvetica", font_), background = "#0000ff", width = wid).grid(row=0, column=1)
+# tk.Entry(master, font=("Helvetica", font_), background = "#ffff00", width = wid).grid(row=1, column=1)
+# tk.Entry(master, font=("Helvetica", font_), background = "#ffffff", width = wid).grid(row=2, column=1)
+# tk.Entry(master, font=("Helvetica", font_), background = "#ff79dc", width = wid).grid(row=3, column=1)
+# tk.Entry(master, font=("Helvetica", font_), background = "#00ffff", width = wid).grid(row=4, column=1)
+
+# # Image with the alphabet, so that the player can relate each number with the letter
+# img2 = ImageTk.PhotoImage(image = Image.open("figures/2_abecedario.png"))
+# image2 = tk.Label(master, image = img2)
+# image2.grid(row=5, column=0, columnspan=1, rowspan=5)
+
+# # Create the text space where the message can be written
+# tk.Text(master, height=1, width=20, font=("Helvetica", font_)).grid(row=11, column=0)
+
+# # # Create the button for when the message is ready
+# # img3 = ImageTk.PhotoImage(Image.open("figures/0_next.png"))
+# # # Define the space for the button
+# # f = tk.Frame(root, height=50, width=50)
+# # f.pack_propagate(0)
+# # f.pack(side = 'right')
+# # # Create button
+# # #command=lambda: retrieve_input() >>> just means do this when i press the button
+# # buttonCommit=tk.Button(f, image = img2, command=lambda: retrieve_input())
+# # buttonCommit.pack(expand=1)
+
+# master.mainloop()
--- Series.py
+++ Series.py
@@ -43,7 +43,7 @@ class Series:
     """
 
     def __init__(self, master):
-        """Initialize the explanation.
+        """Initialize the minigame.
 
         Args:
             master (tkinter.Tk): window where the minigame runs
@@ -51,36 +51,84 @@ class Series:
         # Make sure that the window appears on top
         master.attributes('-topmost', True)
 
-        # Display the numerical series of numbers
-        series = "15 15 10 10  5 \n24 22 20 18 16 \n15 12 9 6 3 \n70 60 50 40 30 \n11 22 33 22 11"
-        w = tk.Message(master, text=series, font=("Helvetica", 32),width = 350)
-        w.pack(side = 'left')
+        font_ = 32; wid = 3
 
-        # Create the text space where the message can be written
-        
-        # textBox=tk.Text(master, height=1, width=20, font=("Helvetica", 32))
-        # textBox.insert("1.0","15 15 10 10 5")
-        # textBox.pack(side = 'left')
+        # Image with the numerical series
+        img1 = ImageTk.PhotoImage(image = Image.open("figures/2_series.png"))
+        image = tk.Label(master, image = img1)
+        image.grid(row=0, column=0, columnspan=1, rowspan=5)
 
-        # # Export and display the explanation image
-        # img1 = ImageTk.PhotoImage(image = Image.open("figures/2_ingenieria.png"))
-        # panel = tk.Label(master, image = img1)
-        # panel.img1 = img1        
-        # panel.pack()
+        # Entries where the player writes the numbers following the series
+        tk.Entry(master, font=("Helvetica", font_), background = "#0000ff", width = wid).grid(row=0, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ffff00", width = wid).grid(row=1, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ffffff", width = wid).grid(row=2, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ff79dc", width = wid).grid(row=3, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#00ffff", width = wid).grid(row=4, column=1)
 
-        # # "Next" button
-        # img2 = ImageTk.PhotoImage(Image.open("figures/0_next.png"))
-        # # Define the space for the button
-        # f = tk.Frame(master, height=50, width=50)
-        # f.pack_propagate(0)
-        # f.pack(side = "right")
-        # # Create button
-        # self.button = tk.Button(f, image = img2, command=f.quit)
-        # self.button.img2 = img2
-        # self.button.pack(expand=1)
+        # Create the button for when the numbers are ready
+        img2 = ImageTk.PhotoImage(Image.open("figures/0_next.png"))
+        tk.Button(master, height=50, width=50, image = img2, command=master.quit).grid(row = 5, column = 1)
         
         # Initialize the window
         master.mainloop()
+
+    def alphabet(self,master):
+
+        font_ = 32; wid = 3
+
+        # Image with the numerical series
+        img1 = ImageTk.PhotoImage(image = Image.open("figures/2_series.png"))
+        image = tk.Label(master, image = img1)
+        image.grid(row=0, column=0, columnspan=1, rowspan=5)
+
+        # Entries where the player writes the numbers following the series
+        tk.Entry(master, font=("Helvetica", font_), background = "#0000ff", width = wid).grid(row=0, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ffff00", width = wid).grid(row=1, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ffffff", width = wid).grid(row=2, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#ff79dc", width = wid).grid(row=3, column=1)
+        tk.Entry(master, font=("Helvetica", font_), background = "#00ffff", width = wid).grid(row=4, column=1)
+
+        # Image with the numerical series
+        img3 = ImageTk.PhotoImage(image = Image.open("figures/2_abecedario.png"))
+        image = tk.Label(master, image = img3)
+        image.grid(row=0, column=2, columnspan=1, rowspan=5)
+
+        # Create the button for when the numbers are ready
+        img4 = ImageTk.PhotoImage(Image.open("figures/0_next.png"))
+        tk.Button(master, height=50, width=50, image = img4, command=master.quit).grid(row = 5, column = 2)
+
+        master.mainloop()
+
+
+class Alphabet:
+    """Minigame where the player needs to complete the numerical series and relate the numbers obtained with the 
+    letters of the alphabet.
+    """
+
+    def __init__(self, master):
+        """Initialize the minigame.
+
+        Args:
+            master (tkinter.Tk): window where the minigame runs
+        """
+        # Make sure that the window appears on top
+        master.attributes('-topmost', True)
+
+
+        # Image with the numerical series
+        img1 = ImageTk.PhotoImage(image = Image.open("figures/2_abecedario.png"))
+        image = tk.Label(master, image = img1)
+        image.grid(row=0, column=0, columnspan=1, rowspan=5)
+
+        # # Create the text space where the message can be written
+        # tk.Text(master, height=1, width=20, font=("Helvetica", 30)).grid(row=5, column=0)
+
+        # # Create the button for when the message is ready
+        # img2 = ImageTk.PhotoImage(Image.open("figures/0_next.png"))
+        # tk.Button(master, height=50, width=50, image = img2, command=master.quit).grid(row = 5, column = 1)
+        
+        # Initialize the window
+        master.mainloop()      
 
 def main_series():
     """Main function which connects all the rest and displays the main directions for the game
@@ -92,8 +140,12 @@ def main_series():
 
     # Start the series game
     root2 = tk.Tk()
-    Series(root2)
-    root2.destroy()
+    s = Series(root2)
+    s.alphabet(root2)
+    # Alphabet(root3)
+
+    # root2.destroy()
+    # root3.destroy()
 
 
 
