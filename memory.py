0e4823175b5a196ae2d13181c8528ba99050e5d8
Alba Alcaniz <albaalcanizmoya@gmail.com>
2020-08-12 19:11:53 +0200

    more optimization

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from math import prod
 from random import shuffle
 from numpy import array, append
@@ -60,21 +61,46 @@ class Card():
         """
         screen.blit(self.text, self.text_rect)
 
+class MainScreen():
+    def __init__(self,screen_size):
+        self.screen = pygame.display.set_mode(screen_size,pygame.RESIZABLE,0,32)
+        self.bg_img = pygame.image.load('figures/6_WinningPicture.jpg')
+        self.bg_img2 = pygame.image.load('figures/6_WinningPicture2.jpg')
 
-def update_screen(img, screen, card_dict):
-    """When a card is turned around or its couple is found, the pygame window 
-    needs to be updated to represent the changes
+    def initial_setup(self,card_dict):
+        pygame.display.update()
+        self.screen.blit(self.bg_img, (0,0))
+        for c in card_dict.values():
+            c[0].draw_card(self.screen)
 
-    Args:
-        img (pygame.Surface): background image
-        screen (pygame.Surface): window where the cards and image are displayed
-        card_dict (dict): dictionary of card objects with its identification
-    """
-    pygame.display.update()
-    screen.blit(img, (0,0))
-    for c in card_dict.values():
-        c[0].draw_card(screen)
+    def new_configuration(self):
+        pygame.display.set_mode([640,616],pygame.RESIZABLE,0,32)
+        self.text = ''
+        self.font = pygame.font.SysFont('calibri',48)
+        self.text_color = (0,0,0)
+        self.img_text = self.font.render(self.text, True, self.text_color)
 
+        self.rect = self.img_text.get_rect()
+        self.rect.topleft = (20, 550)
+        self.cursor = pygame.Rect(self.rect.topright, (3, self.rect.height))
+
+    def modify_text(self,event):
+        if event.key == pygame.K_BACKSPACE:
+            if len(self.text)>0:
+                self.text = self.text[:-1]
+        else:
+            self.text += event.unicode
+
+        self.img_text = self.font.render(self.text, True, self.text_color)
+        self.rect.size=self.img_text.get_size()
+        self.cursor.topleft = self.rect.topright
+
+    def new_setup(self):
+        self.screen.blit(self.bg_img2, (0,0))
+        self.screen.blit(self.img_text, self.rect)
+        if time.time() % 1 > 0.5:
+            pygame.draw.rect(self.screen, self.text_color, self.cursor)
+        pygame.display.update()
 
 def create_cards_dictionary():
     """Function that creates a dictionary of the cards which are identified by
@@ -106,26 +132,23 @@ def main_memory():
     """
     global cards_number, cards_distribution, cards_size
     # Dimensions
-    picture_size = [1920,1280]
-    screen_factor = 3
-    screen_size = [int(x/screen_factor) for x in picture_size]
-    cards_distribution = [6,3]
+    screen_size = [640,426]
+    # cards_distribution = [6,3]
+    cards_distribution = [2,1]
     cards_number = prod(cards_distribution)
     cards_size = [int(x/y) for x, y in zip(screen_size,cards_distribution)]
 
     pygame.init()
-    screen = pygame.display.set_mode(screen_size)
-    img = pygame.image.load('figures/6_WinningPicture.jpg')
-
     card_dict = create_cards_dictionary()
-    
+    screen = MainScreen(screen_size)
+
     while True:
-        update_screen(img, screen, card_dict,False)
         e = pygame.event.wait()
         if e.type == pygame.MOUSEBUTTONDOWN:
             break
         if e.type == pygame.QUIT:
             quit()
+        screen.initial_setup(card_dict)
 
     last_card_id = 0; game = True
     while game:
@@ -138,19 +161,35 @@ def main_memory():
             y_card = int(my/cards_size[1])*cards_size[1]
             pipe_id = str(x_card)+'_'+str(y_card)
             if pipe_id in card_dict.keys():
-                card_dict[pipe_id][0].turn(screen)
+                card_dict[pipe_id][0].turn(screen.screen)
                 if last_card_id != pipe_id and last_card_id != 0 and \
                     card_dict[pipe_id][1] == card_dict[last_card_id][1]:
                         card_dict.pop(pipe_id)
                         card_dict.pop(last_card_id)
                         last_card_id = 0
-                        update_screen(img, screen, card_dict,True)
+                        screen.initial_setup(card_dict)
                 else:
                     last_card_id = pipe_id
-            update_screen(img, screen, card_dict,True)
+            screen.initial_setup(card_dict)
 
         if len(card_dict) == 0:
             game = False
+            
+    screen.new_configuration()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                screen.modify_text(event)
+        screen.new_setup()
+
+        if screen.text.upper() == 'FERNAN':
+            running = False
+            pygame.quit()
 
 
 main_memory()
